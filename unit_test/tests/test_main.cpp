4aebcd1dfb86320d8aa91b8907bf08c315ad4915
KotKymis <DinislamDengi@yandex.ru>
2025-03-23 21:00:24 +0500

    изменения

--- unit_test/tests/test_main.cpp
+++ unit_test/tests/test_main.cpp
@@ -7,7 +7,17 @@ TEST(FuncTest, FuncOkTest) {
     EXPECT_EQ(func_ok(0, 0), 0);
 }
 
-TEST(CustomerCheckTest, CustomerCheckTest) {
-    EXPECT_EQ(customer_check(5), 1);
-    EXPECT_EQ(customer_check(10), 0);
+
+TEST(FuncTest, FuncOkNegativeTest) {
+    EXPECT_EQ(func_ok(-1, -1), 0);  
+    EXPECT_EQ(func_ok(-1, 1), 0);   
+}
+
+TEST(FuncTest, FuncOkLargeNumbersTest) {
+    EXPECT_EQ(func_ok(1000, 1000), 0);  
+    EXPECT_EQ(func_ok(500, 500), 0);    
+}
+
+TEST(FuncTest, FuncOkFailTest) {
+    EXPECT_EQ(func_ok(1, 1), 0);  // Этот тест заведомо не пройдёт, так как func_ok(1, 1) возвращает 1, а мы ожидаем 0
 }
